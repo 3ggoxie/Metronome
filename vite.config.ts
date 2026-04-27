8beb3da1247667434c7304a7894e8b9c0cf210c6
Maxwell <986843038@qq.com>
2026-04-27 22:32:16 +0800

    单文件构建

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
